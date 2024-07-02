88d6fab5d5408b32b6ee3f36e279d4771ca933e1
Kim Min Geun <52354632+mingeun128@users.noreply.github.com>
2024-07-02 19:25:53 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31120 KB, Score: 12 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,14 +3,22 @@ n, k = map(int, sys.stdin.readline().split())
 nara = []
 for _ in range(n):
     inputt = list(map(int, sys.stdin.readline().split()))
-    nara.append((str(inputt[1]) + str(inputt[2]) + str(inputt[3]),inputt[0]))
-nara.sort()
-nara.reverse()
+    nara.append(((inputt[1],inputt[2],inputt[3]),(inputt[0])))
+ranking = sorted(nara, key=lambda x: (-x[0][0],-x[0][1],-x[0][2]))
 answer = 1
 if nara[0][1] != k:
     for i in range(1,n):
-        if nara[i][0] != nara[i-1][0]:
+        if ranking[i][0][0] != ranking[i-1][0][0] or ranking[i][0][1] != ranking[i-1][0][1] or ranking[i][0][2] != ranking[i-1][0][2]:
             answer = i+1
-        if nara[i][1] == k:
+        if ranking[i][1] == k:
             break
+rank = [1]
+for i in range(1,n):
+    if ranking[i][0][0] == ranking[i-1][0][0] and ranking[i][0][1] == ranking[i-1][0][1] and ranking[i][0][2] == ranking[i-1][0][2]:
+        rank.append(rank[i-1])
+    else:
+        rank.append(i+1)
+    if ranking[i][1] == k:
+        answer = i+1
+        break
 print(answer)
